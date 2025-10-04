6acc17e1e23d25d18fc4fb989aca628c5e12d2c3
rnov01 <47936768+rnov01@users.noreply.github.com>
2025-10-04 14:11:52 -0500

    keep pdf report, ignore the rest of the Honors Report folder

--- Honors Report/Quartus Project Files/IP Files/pll/pll_inst.v
+++
@@ -1,6 +0,0 @@
-	pll u0 (
-		.clk_25_clk (<connected-to-clk_25_clk>), // clk_25.clk
-		.clk_in_clk (<connected-to-clk_in_clk>), // clk_in.clk
-		.rst_reset  (<connected-to-rst_reset>)   //    rst.reset
-	);
-
